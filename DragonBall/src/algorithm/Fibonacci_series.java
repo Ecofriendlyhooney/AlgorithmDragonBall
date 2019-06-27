a2a8ccaf10600902ff9e250759b4997fc25c9858
hooney <hooney@10.4.189.225>
2019-06-27 17:24:08 +0900

    till 45th seems fine

--- DragonBall/src/algorithm/Fibonacci_series.java
+++ DragonBall/src/algorithm/Fibonacci_series.java
@@ -2,7 +2,16 @@ package algorithm;
 
 public class Fibonacci_series {
 	public static void main(String args[]) {
+		int a = 1;
+		int b = 1;
+		int c = 1;
 		
+		for(int i=0; i<101; i++) {
+			System.out.println(i + "th n is :" +b);
+			a = b;
+			b = c;
+			c = a+b;
+		}
 	}
 	
 }
